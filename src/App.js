64570cdd3dad2f578ddaa13b72041f6fea321ed1
kyh012412 <kyh0124.12@gmail.com>
2024-04-26 13:10:21 +0900

    프로젝트 시작 / env를 통하여 apikey 접근확인(성공)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const jsKey = process.env.REACT_APP_KAKAO_JAVASCRIPT_API_KEY;
+
+  // console.log(jsKey);
+  if (!window.Kakao.isInitialized()) {
+    // JavaScript key를 인자로 주고 SDK 초기화
+    window.Kakao.init(jsKey);
+    // SDK 초기화 여부를 확인하자.
+    console.log(window.Kakao.isInitialized());
+  }
+  return <h1>kakologin</h1>;
 }
 
 export default App;
